8b219a14473a2eb33fa493ab833d9d2746403aff
Yeonwook Kwon <wook0805@hanmail.net>
2020-01-28 23:48:34 +0900

    14002 문제추가

--- 단계별/DP/14002/LongestSub4.java
+++ 단계별/DP/14002/LongestSub4.java
@@ -1,7 +1,7 @@
 import java.io.*;
 import java.util.*;
 
-public class LongestSub2 {
+public class LongestSub4 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
