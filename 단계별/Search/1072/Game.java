4a04df821260edce5948873c0816e4db04711ca1
Yeonwook Kwon <wook0805@hanmail.net>
2020-02-17 23:52:55 +0900

    1072

--- 단계별/Search/1072/Game.java
+++ 단계별/Search/1072/Game.java
@@ -27,9 +27,6 @@ import java.util.*;
 
 public class Game {
     public static void main(String[] args) throws IOException {
-
-        //System.setIn(new FileInputStream("test.txt"));
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st = new StringTokenizer(br.readLine());
