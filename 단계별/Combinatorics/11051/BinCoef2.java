150b2cf474d278da871c6c8a0c4bf8ca169ffe4e
Yeonwook Kwon <wook0805@hanmail.net>
2020-03-15 00:23:18 +0900

    11051

---
+++ 단계별/Combinatorics/11051/BinCoef2.java
@@ -0,0 +1,41 @@
+/*
+문제
+
+자연수 N과 정수 K가 주어졌을 때 이항계수를 10,007로 나눈 나머지를 구하는 프로그램을 작성하시오.
+
+입력
+
+첫째 줄에 N과 K가 주어진다. (1 ≤ N ≤ 1,000, 0 ≤ K ≤ N)
+
+출력
+
+이항계수를 10,007로 나눈 나머지를 출력한다.
+*/
+
+import java.io.*;
+import java.util.*;
+
+public class BinCoef2 {
+    public static int[][] dp = new int[1001][1001];
+    public static void main(String[] args) throws IOException {
+
+        System.setIn(new FileInputStream("test.txt"));
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+
+        for(int i=1;i<=N;i++){
+            for(int j=0;j<=K;j++){
+                if(j == 0 || j == i)
+                    dp[i][j] = 1;
+                else
+                    dp[i][j] = (dp[i-1][j-1] + dp[i-1][j]) % 10007;
+            }
+        }
+        bw.write(dp[N][K] + "\n");
+        bw.close();
+    }
+}
