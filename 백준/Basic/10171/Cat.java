1a753c3373cdbe0aad867cbe6b2f28d3b89521ea
grappe96 <w00k0805@naver.com>
2021-08-25 18:19:00 +0900

    예전 폴더 삭제

--- 백준/Basic/10171/Cat.java
+++
@@ -1,8 +0,0 @@
-public class Cat {
-    public static void main(String[] args) {
-        System.out.println("\\    /\\\n"+
-                            " )  ( ')\n"+
-                            "(  /  )\n"+
-                            " \\(__)|");
-    }
-}
