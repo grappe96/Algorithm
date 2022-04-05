bb2db9b832106d6ad64fdcc5dec12f396c8c9780
grappe96 <w00k0805@naver.com>
2022-04-05 23:51:58 +0900

    leetcode Divide Two Integers

---
+++ JS/leetcode/DivideTwoIntegers.js
@@ -0,0 +1,27 @@
+var divide = function (dividend, divisor) {
+  let quotient = 0;
+  const max = Math.pow(2, 31),
+    check = divisor < 0 === dividend < 0;
+
+  dividend = Math.abs(dividend);
+  divisor = Math.abs(divisor);
+
+  if (divisor === 1) quotient = dividend;
+  else {
+    while (dividend >= divisor) {
+      let tmpSor = divisor,
+        tmpEnd = dividend >> 1,
+        i = 1;
+      while (tmpSor <= tmpEnd) {
+        i <<= 1;
+        tmpSor <<= 1;
+      }
+      dividend -= tmpSor;
+      quotient += i;
+    }
+  }
+
+  quotient = check ? -quotient : quotient;
+  quotient = Math.min(max - 1, Math.max(-max, quotient));
+  return quotient;
+};
