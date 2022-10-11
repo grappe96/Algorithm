d3a7b378d285a007b9ba70d868b4b4231e3615a6
grappe96 <w00k0805@naver.com>
2022-10-12 01:16:28 +0900

    프로그래머스 단속카메라 JAVA 수정

--- Java/Programmers/Solution_단속카메라.java
+++ Java/Programmers/Solution_단속카메라.java
@@ -4,21 +4,20 @@ import java.util.Arrays;
 
 class Solution {
     public int solution(int[][] routes) {
-        int answer = 1, start, end, len = routes.length;
+        int answer = 1, end, len = routes.length;
         
         Arrays.sort(routes, (i1, i2) -> {
             if(i1[0] == i2[0])
                 return i1[1] - i2[1];
             return i1[0] - i2[0];
         });
-        start = routes[0][0];
         end = routes[0][1];
+        
         for(int i=1;i<len;i++){
             if (routes[i][0] <= end)
                 end = Math.min(end, routes[i][1]);
             else {
                 answer++;
-                start = Math.max(start, routes[i][0]);
                 end = routes[i][1];
             }
         }
